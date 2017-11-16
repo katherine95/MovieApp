b501df95f14ea740218cc543e31590798b149541
katherine95 <kathiekim95@gmail.com>
2017-11-16 23:23:42 +0300

    (feature)display actors belonging to a movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,11 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
-  end  
+  end
+
+  def show
+    @movie=Movie.find(params[:id])
+    @actors=@movie.actors
+  end
 
 end
